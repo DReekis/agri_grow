bd1e37803419838450f72653d580bfea72569aa5
DReejis <rickghosh86@gmail.com>
2024-09-08 23:00:21 +0530

    1st

---
+++ script.js
@@ -0,0 +1,11 @@
+document.getElementById('upload-photo').addEventListener('click', function() {
+    window.location.href = 'upload.html'; // Redirect to photo upload page
+});
+
+document.getElementById('crop-prices').addEventListener('click', function() {
+    window.location.href = 'prices.html'; // Redirect to future crop prices page
+});
+
+document.getElementById('crop-recommendation').addEventListener('click', function() {
+    window.location.href = 'recommendation.html'; // Redirect to crop recommendation page
+});
